1c8a54bb20c764f13fb8cacd0efe1b67e924f3ae
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2025-01-03 13:09:09 +0000

    feat: enhance application.js with CSRF token setup and remove deadlock retry logic from ApplicationJob

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+console.log('Hello from Import Maps');
+// filepath: /c:/bluefolder/app/javascript/packs/application.js
+document.addEventListener("DOMContentLoaded", () => {
+    const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    $.ajaxSetup({
+      headers: {
+        'X-CSRF-Token': token
+      }
+    });
+  });
