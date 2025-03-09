cd4a89997eb08b958e828b3db9c799827791e1bb
Evans kofi Nyamekye <nyamekye2131@gmail.com>
2025-03-09 11:15:50 +0000

    Create dom.js

---
+++ app/javascript/dom.js
@@ -0,0 +1,7 @@
+cont itemForm = document.getElementaById ('item-form');
+cont itemInput = document.getElementaById ('item-input');
+cont itemList = document.getElementaById ('item-list'); 
+
+// let's add a validation and create a function 
+
+
